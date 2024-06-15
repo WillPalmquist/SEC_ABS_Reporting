59889482658d093658a78cd05d066a6c21716234
Will Palmquist <willpalmquist@Wills-MacBook-Pro.local>
2024-06-15 13:04:06 -0600

    quarto report

--- src/03-Forecasting.R
+++ src/03-Forecasting.R
@@ -12,6 +12,18 @@ library(forecast)
 ############################################
 # !!!Load data in 02-Exploratory-Analysis.R
 ###########################################
+abs_ts_sums <- abs_ee_data_clean |> 
+  dplyr::group_by(reportingPeriodEndingDate) |> 
+  dplyr::summarise(
+    dplyr::across(c(contains("amount"), contains("percentage"), remainingTermToMaturityNumber, obligorCreditScore),
+                  ~ sum(.x, na.rm = T)),
+    numberActiveLoans = sum(remainingTermToMaturityNumber > 0 & is.na(zeroBalanceCode), na.rm = T),
+    numberNewPrepaidLoans = sum(prepaidFlagFirst,na.rm = T),
+    numberPrepaidLoansTotal = sum(prepaidFlag, na.rm = T),
+    defaults = sum(zeroBalanceCode == 4, na.rm = T),
+    prepayments = sum(zeroBalanceCode == 1, na.rm = T)
+  ) |> 
+  dplyr::ungroup()
 
 #####################
 ### Method 1
@@ -20,10 +32,10 @@ library(forecast)
 cum_charged_off_principal <- sum(as.numeric(abs_ee_data$chargedoffPrincipalAmount), na.rm = T)
 cum_charged_off_principal
 #### Forecasting
-charged_off_principal_ts <- abs_ee_data_grouped |> 
-  dplyr::select(reportingPeriodEndingDate,chargedoffPrincipalAmount_sum,numberActiveLoans,numberNewPrepaidLoans,numberPrepaidLoansTotal) |> 
+charged_off_principal_ts <- abs_ts_sums |> 
+  dplyr::select(reportingPeriodEndingDate,chargedoffPrincipalAmount,numberActiveLoans,numberNewPrepaidLoans,numberPrepaidLoansTotal) |> 
   dplyr::mutate(
-    chargedoffPrincipalAmountCumulative = cumsum(chargedoffPrincipalAmount_sum)
+    chargedoffPrincipalAmountCumulative = cumsum(chargedoffPrincipalAmount)
   )
 
 ### Model Fitting
@@ -61,45 +73,48 @@ data.frame(
 #####################
 ### Method 2
 #####################
-df_chargeoffs <- abs_ee_data_grouped |> 
-  dplyr::select(reportingPeriodEndingDate, chargedoffPrincipalAmount_sum, reportingPeriodActualEndBalanceAmount_sum) |> 
+df_chargeoffs <- abs_ts_sums |> 
+  dplyr::select(reportingPeriodEndingDate, chargedoffPrincipalAmount, reportingPeriodActualEndBalanceAmount) |> 
   dplyr::mutate(
-    chargeOffPercentOfBalance = chargedoffPrincipalAmount_sum/reportingPeriodActualEndBalanceAmount_sum,
-    EndBalanceAmountYOY = (reportingPeriodActualEndBalanceAmount_sum/lag(reportingPeriodActualEndBalanceAmount_sum))-1,
-    period = "Actual"
+    chargeOffPercentOfBalance = chargedoffPrincipalAmount/reportingPeriodActualEndBalanceAmount,
+    EndBalanceAmountYOY = (reportingPeriodActualEndBalanceAmount/lag(reportingPeriodActualEndBalanceAmount))-1,
+    Period = "Actual"
     )
 
 stableBalanceGrowthRate = mean(df_chargeoffs |> tail(12) |> dplyr::pull(EndBalanceAmountYOY),na.rm =T)
 stableChargeOffPercent = mean(df_chargeoffs |> tail(12) |> dplyr::pull(chargeOffPercentOfBalance),na.rm =T)
 
-last_balance <- df_chargeoffs |> pull(reportingPeriodActualEndBalanceAmount_sum) |> tail(1)
+last_balance <- df_chargeoffs |> pull(reportingPeriodActualEndBalanceAmount) |> tail(1)
 
 df_forecast <- data.frame(
   reportingPeriodEndingDate = seq.Date(from = max(df_chargeoffs$reportingPeriodEndingDate) %m+% months(1), by = "month",length.out = 17),
-  reportingPeriodActualEndBalanceAmount_sum = last_balance *cumprod(rep(1+stableBalanceGrowthRate,17))
+  reportingPeriodActualEndBalanceAmount = last_balance *cumprod(rep(1+stableBalanceGrowthRate,17))
 ) |> 
   dplyr::mutate(
-    chargedoffPrincipalAmount_sum = reportingPeriodActualEndBalanceAmount_sum * stableChargeOffPercent,
-    period = "Forecast"
+    chargedoffPrincipalAmount = reportingPeriodActualEndBalanceAmount * stableChargeOffPercent,
+    Period = "Forecast"
   )
 
 combined_forecast <- df_chargeoffs |> 
   dplyr::bind_rows(df_forecast) |> 
   dplyr::mutate(
-    cumulativeSumChargeOffs = cumsum(chargedoffPrincipalAmount_sum)
+    cumulativeSumChargeOffs = cumsum(chargedoffPrincipalAmount)
   )
 
-CumulativeChargeOffsMonth19 <- sum(combined_forecast$chargedoffPrincipalAmount_sum[combined_forecast$period == "Actual"], na.rm = T)
-CumulativeChargeOffsMonth36 <- sum(combined_forecast$chargedoffPrincipalAmount_sum, na.rm = T)
+CumulativeChargeOffsMonth19 <- sum(combined_forecast$chargedoffPrincipalAmount[combined_forecast$Period == "Actual"], na.rm = T)
+CumulativeChargeOffsMonth36 <- sum(combined_forecast$chargedoffPrincipalAmount, na.rm = T)
+
+ggplot2::ggplot(combined_forecast) +
+  ggplot2::geom_line(ggplot2::aes(x = reportingPeriodEndingDate, y = chargedoffPrincipalAmount, color = Period), size = 1) +
+  ggplot2::theme_minimal()
+
+ggplot2::ggplot(combined_forecast) +
+  ggplot2::geom_line(ggplot2::aes(x = reportingPeriodEndingDate, y = reportingPeriodActualEndBalanceAmount, color = Period), size = 1) +
+  ggplot2::theme_minimal()
 
 ggplot(combined_forecast) +
-  # geom_line(aes(x = reportingPeriodEndingDate, y = reportingPeriodActualEndBalanceAmount_sum, colour = "Loan Balance Amount", color = period), size = 1)+
-  geom_line(aes(x = reportingPeriodEndingDate, y = chargedoffPrincipalAmount_sum, colour = "chargedoffPrincipalAmount", color = period), size = 1)
+  ggplot2::geom_line(ggplot2::aes(x = reportingPeriodEndingDate, y = cumulativeSumChargeOffs, color = Period), size = 1) +
+  ggplot2::theme_minimal()
 
-ggplot(combined_forecast) +
-  geom_line(aes(x = reportingPeriodEndingDate, y = reportingPeriodActualEndBalanceAmount_sum, colour = "Loan Balance Amount", color = period), size = 1)
-
-ggplot(combined_forecast) +
-  geom_line(aes(x = reportingPeriodEndingDate, y = cumulativeSumChargeOffs, colour = "Cumulative Charged Off Principal", color = period), size = 1)
 
 
